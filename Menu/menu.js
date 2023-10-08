deeb9483afc461ce0bdb95911d9b417fac8aa38b
magvtv <magutujr@gmail.com>
2023-10-08 15:40:02 +0300

    bug in modifying menu icon to change on click

--- Menu/menu.js
+++ Menu/menu.js
@@ -4,6 +4,12 @@ function showSidebar (toggleId, sidebarId) {
     if(toggle && sidebar) {
         toggle.addEventListener('click', () => {
             sidebar.classList.toggle('show-sidebar')
+            
+            //toggle menu icon visibility 
+            const menuIcon = toggle.getElementById('open-menu')
+            const closeIcon = toggle.getElementById('close-menu')
+            menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none'
+            closeIcon.style.display = closeIcon.style.display === 'none' ? 'block' : 'none'
         })
     }
 }
