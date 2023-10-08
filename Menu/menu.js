6f05d3f610d1191a87ec8fd7445112096eaffbc7
magvtv <magutujr@gmail.com>
2023-10-08 14:06:48 +0300

    seamless working of the sidenav on mobile rendition, to add media breakpoints

---
+++ Menu/menu.js
@@ -0,0 +1,10 @@
+function showSidebar (toggleId, sidebarId) {
+    const toggle = document.getElementById(toggleId), sidebar = document.getElementById(sidebarId)
+
+    if(toggle && sidebar) {
+        toggle.addEventListener('click', () => {
+            sidebar.classList.toggle('show-sidebar')
+        })
+    }
+}
+showSidebar('header-toggle', 'sidebar')
