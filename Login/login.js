e94732ba6d5605976ccc122d3be90b6abaedb81b
magvtv <magutujr@gmail.com>
2023-10-18 08:08:21 +0300

    similar to signup, need to modify the code

---
+++ Login/login.js
@@ -0,0 +1,61 @@
+const form = document.getElementsByClassName("signup__form");
+// deactivate 'create account' until all details have been filled in
+document.addEventListener("DOMContentLoaded", () => {
+  const email = document.getElementById("signup-email");
+  const signUpButton = document.getElementById("signup-button");
+
+  // make sure the password above is the same as the one below
+//   const doPasswordsMatch = () => {
+//     return passwordOne === passwordTwo;
+//   };
+
+  const isFormValid = () => {
+    const isEmailValid = email.checkValidity();
+    const isPasswordOneValid = passwordOne.checkValidity();
+    const isPasswordTwoValid = passwordTwo.checkValidity();
+    return (
+      isEmailValid &&
+      isPasswordOneValid &&
+      isPasswordTwoValid 
+    //   doPasswordsMatch()
+    );
+  };
+
+  const updateButtonState = () => {
+    signUpButton.disabled = !isFormValid();
+  };
+
+  email.addEventListener("input", updateButtonState);
+  passwordOne.addEventListener("input", updateButtonState);
+  passwordTwo.addEventListener("input", updateButtonState);
+
+  updateButtonState();
+//   form.addEventListener("submit", (go) => {
+//     if (!doPasswordsMatch()) {
+//       go.preventDefault();
+//       alert("Your passwords do not match!");
+//     }
+//   });
+
+});
+
+// show passcode with clicking eye icon. could i implement for both password fields?
+const showPassword = (signUpPassword, passwordEye) => {
+  const input = document.getElementById(signUpPassword),
+  eyeIcon = document.getElementById(passwordEye);
+
+  eyeIcon.addEventListener("click", () => {
+    // change the password to plain text
+    if (input.type === "password") {
+      input.type = "text";
+      eyeIcon.classList.add("ri-eye-line");
+      eyeIcon.classList.remove("ri-eye-close-line");
+    } else {
+      input.type = "password";
+      eyeIcon.classList.add("ri-eye-close-line");
+      eyeIcon.classList.remove("ri-eye-line");
+    }
+  });
+};
+
+showPassword("login-pass", "password-eye");
