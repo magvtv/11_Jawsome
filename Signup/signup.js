010b272770d7efca91451b2c88a7aaec95929b7c
magvtv <magutujr@gmail.com>
2023-10-18 06:35:26 +0300

    done recommended breakpoints, finalizing signup.js

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,6 +1,5 @@
 // deactivate 'create account' until all details have been filled in
 // make sure the password above is the same as the one below
-
 // show passcode with eye
 const showPassword = (signupPassword, passwordEye) => {
   const input = document.getElementById(signupPassword),
