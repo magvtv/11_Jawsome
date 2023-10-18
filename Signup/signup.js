42c23bf3afd82be60f693b205c533f3e61e305be
magvtv <magutujr@gmail.com>
2023-10-18 07:14:56 +0300

    disabled button feature until all fields are filled in

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,8 +1,34 @@
+const passwordOne = document.getElementById("signup-pass1");
+const passwordTwo = document.getElementById("signup-pass2");
 // deactivate 'create account' until all details have been filled in
+
+document.addEventListener("DOMContentLoaded", () => {
+  const email = document.getElementById("signup-email");
+  const signUpButton = document.getElementById("signup-button");
+
+  const isFormValid = () => {
+    const isEmailValid = email.checkValidity();
+    const isPasswordOneValid = passwordOne.checkValidity();
+    const isPasswordTwoValid = passwordTwo.checkValidity();
+    return isEmailValid && isPasswordOneValid && isPasswordTwoValid;
+  };
+
+  const updateButtonState = () => {
+    signUpButton.disabled = !isFormValid();
+  };
+
+  email.addEventListener("input", updateButtonState);
+  passwordOne.addEventListener("input", updateButtonState);
+  passwordTwo.addEventListener("input", updateButtonState);
+
+  updateButtonState();
+});
+
 // make sure the password above is the same as the one below
+
 // show passcode with eye
-const showPassword = (signupPassword, passwordEye) => {
-  const input = document.getElementById(signupPassword),
+const showPassword = (signUpPassword, passwordEye) => {
+  const input = document.getElementById(signUpPassword),
     eyeIcon = document.getElementById(passwordEye);
 
   eyeIcon.addEventListener("click", () => {
@@ -19,4 +45,4 @@ const showPassword = (signupPassword, passwordEye) => {
   });
 };
 
-showPassword("signup-pass", "password-eye");
+showPassword("signup-pass2", "password-eye");
