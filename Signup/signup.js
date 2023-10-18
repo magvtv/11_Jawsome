15f1045f82317399aa1b0904d68fe8d0586cdbf9
magvtv <magutujr@gmail.com>
2023-10-18 07:48:27 +0300

    bugs with doPasswordsMatch and updateButtonState

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,16 +1,26 @@
+const form = document.getElementsByClassName("signup__form");
 const passwordOne = document.getElementById("signup-pass1");
 const passwordTwo = document.getElementById("signup-pass2");
 // deactivate 'create account' until all details have been filled in
-
 document.addEventListener("DOMContentLoaded", () => {
   const email = document.getElementById("signup-email");
   const signUpButton = document.getElementById("signup-button");
+
+  // make sure the password above is the same as the one below
+//   const doPasswordsMatch = () => {
+//     return passwordOne === passwordTwo;
+//   };
 
   const isFormValid = () => {
     const isEmailValid = email.checkValidity();
     const isPasswordOneValid = passwordOne.checkValidity();
     const isPasswordTwoValid = passwordTwo.checkValidity();
-    return isEmailValid && isPasswordOneValid && isPasswordTwoValid;
+    return (
+      isEmailValid &&
+      isPasswordOneValid &&
+      isPasswordTwoValid &&
+    //   doPasswordsMatch()
+    );
   };
 
   const updateButtonState = () => {
@@ -22,27 +32,33 @@ document.addEventListener("DOMContentLoaded", () => {
   passwordTwo.addEventListener("input", updateButtonState);
 
   updateButtonState();
+//   form.addEventListener("submit", (go) => {
+//     if (!doPasswordsMatch()) {
+//       go.preventDefault();
+//       alert("Your passwords do not match!");
+//     }
+//   });
+
 });
 
-// make sure the password above is the same as the one below
-
-// show passcode with eye
-const showPassword = (signUpPassword, passwordEye) => {
-  const input = document.getElementById(signUpPassword),
-    eyeIcon = document.getElementById(passwordEye);
+// show passcode with clicking eye icon. could i implement for both password fields?
+const showPassword = (signUpPasswordOne, signUpPasswordTwo, passwordEye) => {
+  const inputOne = document.getElementById(signUpPasswordOne),
+    inputTwo = document.getElementById(signUpPasswordTwo);
+  eyeIcon = document.getElementById(passwordEye);
 
   eyeIcon.addEventListener("click", () => {
     // change the password to plain text
-    if (input.type === "password") {
-      input.type = "text";
+    if (inputOne.type === "password") {
+      inputOne.type = "text";
       eyeIcon.classList.add("ri-eye-line");
       eyeIcon.classList.remove("ri-eye-close-line");
     } else {
-      input.type = "password";
+      inputOne.type = "password";
       eyeIcon.classList.add("ri-eye-close-line");
       eyeIcon.classList.remove("ri-eye-line");
     }
   });
 };
 
-showPassword("signup-pass2", "password-eye");
+showPassword("signup-pass1", "signup-pass2", "password-eye");
