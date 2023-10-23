18149b5cafd90c21939667530f06e0f214cbaed5
magvtv <magutujr@gmail.com>
2023-10-23 07:24:41 +0300

    added other background photos, deleted chat logo, still stuck with the menu toggle function

--- Menu/menu.js
+++ Menu/menu.js
@@ -1,25 +1,27 @@
-function showSidebar (toggleId, sidebarId) {
-    const toggle = document.getElementById(toggleId), 
-    sidebar = document.getElementById(sidebarId), 
-    menuIcon = toggle.getElementById('open-menu'), 
-    closeIcon = toggle.getElementById('close-menu')
+document.addEventListener('DOMContentLoaded', () => {
+	function showSidebar(toggleId, sidebarId) {
+		const toggle = document.getElementById(toggleId),
+			sidebar = document.getElementById(sidebarId),
+			menuIcon = toggle.getElementById('open-menu'),
+			closeIcon = toggle.getElementById('close-menu');
 
-    if(toggle && sidebar && menuIcon && closeIcon) {
-        toggle.addEventListener('click', () => {
-            sidebar.classList.toggle('show-sidebar')
+		if (toggle && sidebar) {
+			toggle.addEventListener('click', () => {
+				sidebar.classList.toggle('show-sidebar');
 
-            // if (menuIcon.style.display === 'none') {
-            //     menuIcon.style.display = 'block'
-            //     closeIcon.style.display = 'none'
-            // } else {
-            //     menuIcon.style.display = 'none'
-            //     closeIcon.style.display = 'block'
-            // }
-            
-            //toggle menu icon visibility 
-            menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none'
-            closeIcon.style.display = closeIcon.style.display === 'none' ? 'block' : 'none'
-        })
-    }
-}
-showSidebar('header-toggle', 'sidebar')
+				// if (menuIcon.style.display === 'none') {
+				//     menuIcon.style.display = 'block'
+				//     closeIcon.style.display = 'none'
+				// } else {
+				//     menuIcon.style.display = 'none'
+				//     closeIcon.style.display = 'block'
+				// }
+
+				//toggle menu icon visibility
+				menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none';
+				closeIcon.style.display = closeIcon.style.display === 'none' ? 'block' : 'none';
+			});
+		}
+	}
+	showSidebar('header-toggle', 'sidebar');
+});
