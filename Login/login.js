64bfc2d45cd874be3b015b38602325b2a233ec4d
magvtv <magutujr@gmail.com>
2023-10-23 05:02:50 +0300

    one slight bug in login (negligible)

--- Login/login.js
+++ Login/login.js
@@ -1,38 +1,7 @@
-const form = document.getElementsByClassName("login__form");
-// deactivate 'create account' until all details have been filled in
+// deactivate 'login account' until all details have been filled in
 document.addEventListener("DOMContentLoaded", () => {
-  const email = document.getElementById("login-email");
-  const password = document.getElementById('login-pass')
-  const logInButton = document.getElementById("login-button");
 
-
-  const isFormValid = () => {
-    const isEmailValid = email.checkValidity();
-    const isPasswordValid = password.checkValidity();
-    return (
-      isEmailValid &&
-      isPasswordValid
-    );
-  };
-
-  const updateButtonState = () => {
-    logInButton.disabled = !isFormValid();
-  };
-
-  email.addEventListener("input", updateButtonState);
-  password.addEventListener("input", updateButtonState);
-
-  // updateButtonState();
-//   form.addEventListener("submit", (go) => {
-//     if (!doPasswordsMatch()) {
-//       go.preventDefault();
-//       alert("Your passwords do not match!");
-//     }
-//   });
-
-});
-
-// show passcode with clicking eye icon. could i implement for both password fields?
+  // show passcode with clicking eye icon. could i implement for both password fields?
 const showPassword = (logInPassword, passwordEye) => {
   const input = document.getElementById(logInPassword),
   eyeIcon = document.getElementById(passwordEye);
@@ -52,3 +21,40 @@ const showPassword = (logInPassword, passwordEye) => {
 };
 
 showPassword("login-pass", "password-eye");
+
+  const logInEmail = document.getElementById("login-email");
+  const logInPassword = document.getElementById('login-pass')
+  const logInButton = document.getElementById("login-button");
+
+
+  const isFormValid = () => {
+    const isEmailValid = logInEmail.checkValidity();
+    const isPasswordValid = logInPassword.checkValidity();
+
+    const isNotEmpty = (field) => {
+      field.value.trim() !== ""
+    }
+    return (
+      isEmailValid &&
+      isPasswordValid &&
+      isNotEmpty(logInEmail) &&
+      isNotEmpty(logInPassword)
+    );
+  };
+
+  const updateButtonState = () => {
+    logInButton.disabled = !isFormValid();
+  };
+
+  logInEmail.addEventListener("input", updateButtonState);
+  logInPassword.addEventListener("input", updateButtonState);
+
+  const form = document.getElementsByClassName("login__form");
+  form.addEventListener("submit", (go) => {
+    if (!isFormValid()) {
+      go.preventDefault();
+      alert("Enter both password and email!");
+    }
+  });
+
+});
--- Signup/signup.js
+++ Signup/signup.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // make sure the password above is the same as the one below
   const doPasswordsMatch = () => {
-    const passwordOneValue = passwordOne.value.trim()
+    const passwordOneValue = passwordOne.value.trim();
     const passwordTwoValue = passwordTwo.value.trim();
 
-    if (passwordOneValue === '' || passwordTwoValue === '') {
-      return false
+    if (passwordOneValue === "" || passwordTwoValue === "") {
+      return false;
     }
 
     return passwordOneValue === passwordTwoValue;
@@ -44,26 +44,26 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Your passwords do not match!");
     }
   });
+
+  // show passcode with clicking eye icon. could i implement for both password fields?
+  const showPassword = (signUpPassword, passwordEye) => {
+    const input = document.getElementById(signUpPassword),
+      eyeIcon = document.getElementById(passwordEye);
+
+    eyeIcon.addEventListener("click", () => {
+      // change the password to plain text
+      if (input.type === "password") {
+        input.type = "text";
+        eyeIcon.classList.add("ri-eye-close-line");
+        eyeIcon.classList.remove("ri-eye-line");
+      } else {
+        input.type = "password";
+        eyeIcon.classList.add("ri-eye-line");
+        eyeIcon.classList.remove("ri-eye-close-line");
+      }
+    });
+  };
+
+  showPassword("signup-pass1", "password-eye1");
+  showPassword("signup-pass2", "password-eye2");
 });
-
-// show passcode with clicking eye icon. could i implement for both password fields?
-const showPassword = (signUpPassword, passwordEye) => {
-  const input = document.getElementById(signUpPassword),
-    eyeIcon = document.getElementById(passwordEye);
-
-  eyeIcon.addEventListener("click", () => {
-    // change the password to plain text
-    if (input.type === "password") {
-      input.type = "text";
-      eyeIcon.classList.add("ri-eye-close-line");
-      eyeIcon.classList.remove("ri-eye-line");
-    } else {
-      input.type = "password";
-      eyeIcon.classList.add("ri-eye-line");
-      eyeIcon.classList.remove("ri-eye-close-line");
-    }
-  });
-};
-
-showPassword("signup-pass1", "password-eye1");
-showPassword("signup-pass2", "password-eye2");
