b8a06e0ae340a8580f8113b95b03b8b07ad3fc4b
magvtv <magutujr@gmail.com>
2023-10-23 08:07:17 +0300

    accepted changes from my stash

--- Login/login.js
+++ Login/login.js
@@ -1,25 +1,52 @@
-// deactivate 'login account' until all details have been filled in
-document.addEventListener("DOMContentLoaded", () => {
+const form = document.getElementsByClassName('login__form');
+// deactivate 'create account' until all details have been filled in
+document.addEventListener('DOMContentLoaded', () => {
+	const email = document.getElementById('login-email');
+	const password = document.getElementById('login-pass');
+	const logInButton = document.getElementById('login-button');
 
-  // show passcode with clicking eye icon. could i implement for both password fields?
+	const isFormValid = () => {
+		const isEmailValid = email.checkValidity();
+		const isPasswordValid = password.checkValidity();
+		return isEmailValid && isPasswordValid;
+	};
+
+	const updateButtonState = () => {
+		logInButton.disabled = !isFormValid();
+	};
+
+	email.addEventListener('input', updateButtonState);
+	password.addEventListener('input', updateButtonState);
+
+	// updateButtonState();
+	//   form.addEventListener("submit", (go) => {
+	//     if (!doPasswordsMatch()) {
+	//       go.preventDefault();
+	//       alert("Your passwords do not match!");
+	//     }
+	//   });
+});
+
+// show passcode with clicking eye icon. could i implement for both password fields?
 const showPassword = (logInPassword, passwordEye) => {
-  const input = document.getElementById(logInPassword),
-  eyeIcon = document.getElementById(passwordEye);
+	const input = document.getElementById(logInPassword),
+		eyeIcon = document.getElementById(passwordEye);
 
-  eyeIcon.addEventListener("click", () => {
-    // change the password to plain text
-    if (input.type === "password") {
-      input.type = "text";
-      eyeIcon.classList.add("ri-eye-close-line");
-      eyeIcon.classList.remove("ri-eye-line");
-    } else {
-      input.type = "password";
-      eyeIcon.classList.add("ri-eye-line");
-      eyeIcon.classList.remove("ri-eye-close-line");
-    }
-  });
+	eyeIcon.addEventListener('click', () => {
+		// change the password to plain text
+		if (input.type === 'password') {
+			input.type = 'text';
+			eyeIcon.classList.add('ri-eye-close-line');
+			eyeIcon.classList.remove('ri-eye-line');
+		} else {
+			input.type = 'password';
+			eyeIcon.classList.add('ri-eye-line');
+			eyeIcon.classList.remove('ri-eye-close-line');
+		}
+	});
 };
 
+<<<<<<< Updated upstream
 showPassword("login-pass", "password-eye");
 
   const logInEmail = document.getElementById("login-email");
@@ -58,3 +85,6 @@ showPassword("login-pass", "password-eye");
   });
 
 });
+=======
+showPassword('login-pass', 'password-eye');
+>>>>>>> Stashed changes
