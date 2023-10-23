ffd02385ef09cf7037a69b71138e44687e22af98
magvtv <magutujr@gmail.com>
2023-10-23 06:55:59 +0300

    added dark chat background, issue with the menu icon and menu close icon

--- Menu/menu.js
+++ Menu/menu.js
@@ -1,13 +1,22 @@
 function showSidebar (toggleId, sidebarId) {
-    const toggle = document.getElementById(toggleId), sidebar = document.getElementById(sidebarId)
+    const toggle = document.getElementById(toggleId), 
+    sidebar = document.getElementById(sidebarId), 
+    menuIcon = toggle.getElementById('open-menu'), 
+    closeIcon = toggle.getElementById('close-menu')
 
-    if(toggle && sidebar) {
+    if(toggle && sidebar && menuIcon && closeIcon) {
         toggle.addEventListener('click', () => {
             sidebar.classList.toggle('show-sidebar')
+
+            // if (menuIcon.style.display === 'none') {
+            //     menuIcon.style.display = 'block'
+            //     closeIcon.style.display = 'none'
+            // } else {
+            //     menuIcon.style.display = 'none'
+            //     closeIcon.style.display = 'block'
+            // }
             
             //toggle menu icon visibility 
-            const menuIcon = toggle.getElementById('open-menu')
-            const closeIcon = toggle.getElementById('close-menu')
             menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none'
             closeIcon.style.display = closeIcon.style.display === 'none' ? 'block' : 'none'
         })
